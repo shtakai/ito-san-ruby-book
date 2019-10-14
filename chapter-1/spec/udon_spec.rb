ff631f31adcdf0eba3e80d2ad2442ace11fd64d8
Alyson Takai <shtakai@gmail.com>
2019-10-14 11:40:32 +0900

    Add simple test fuck off

---
+++ chapter-1/spec/udon_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_relative './spec_helper'
+
+RSpec.describe 'Udon' do
+  describe 'udon' do
+    it 'is okay' do
+      expect(1).to eq(1)
+    end
+  end
+end
