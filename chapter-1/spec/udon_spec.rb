6dcaca4ffcb4a40aa53f4a0e268f4c640dfcd4f9
Alyson Takai <shtakai@gmail.com>
2019-10-14 13:07:40 +0900

    Add specs for actual udon

--- chapter-1/spec/udon_spec.rb
+++ chapter-1/spec/udon_spec.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 require_relative './spec_helper'
+require_relative '../lib/udon'
 
-RSpec.describe 'Udon' do
-  describe 'udon' do
-    it 'is okay' do
-      expect(1).to eq(1)
+RSpec.describe 'udon' do
+  describe '#udon' do
+    context '1人前以上' do
+      it { expect(udon(amount: 1)).to eq('うどん 1人前') }
+      it { expect(udon(amount: 100)).to eq('うどん 100人前') }
+    end
+
+    context '0人前以下' do
+      it { expect(udon).to eq('うどん売り切れ') }
+      it { expect(udon(amount: -10)).to eq('うどん売り切れ') }
+    end
+
+    context 'amount: nil' do
+      it { expect(udon(amount: nil)).to be_nil }
     end
   end
 end
